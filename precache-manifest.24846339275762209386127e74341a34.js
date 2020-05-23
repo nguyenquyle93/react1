5aba3aad84a84035c82a5f7edb33f839433b89cc
Nguyễn Quý Lễ <nguyenquyle93@gmail.com>
2020-05-23 20:47:00 +0700

    Upload file code lan dau

---
+++ precache-manifest.24846339275762209386127e74341a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970b3860f313cef4faf74f41d2d37b7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83bbcc0c70f814cea9cd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30424d07fff7ef84d084",
+    "url": "/static/js/2.8e037379.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e037379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bbcc0c70f814cea9cd",
+    "url": "/static/js/main.c78ea7bb.chunk.js"
+  },
+  {
+    "revision": "d15bb7dfa29c9d9f99fa",
+    "url": "/static/js/runtime-main.6e8c4752.js"
+  }
+]);
